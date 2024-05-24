83730c5710aba112b5a278ac0717202eec4a9513
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-24 12:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+        function sortArticles(articles) {
+            const excludedWords = ['a', 'an', 'the'];
+
+            return articles.sort((a, b) => {
+                const aTitle = a.replace(new RegExp(`\\b(?:${excludedWords.join('|')})\\b`, 'gi'), '').trim();
+                const bTitle = b.replace(new RegExp(`\\b(?:${excludedWords.join('|')})\\b`, 'gi'), '').trim();
+                return aTitle.localeCompare(bTitle, 'en', { sensitivity: 'base' });
+            });
+        }
+
+        const sortedArticles = sortArticles(bands);
+
+        const list = document.getElementById('band');
+        sortedArticles.forEach(article => {
+            const li = document.createElement('li');
+            li.textContent = article;
+            list.appendChild(li);
+        });
